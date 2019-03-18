0a78adb69fe74198f11f728a7fb4a3441cc9b819
Kirk Boyd <kirkland.boyd@trincoll.edu>
2019-03-18 15:49:37 +0000

    fix: complete python file to properly run trajectory

--- src/exercise_3_1.py
+++ src/exercise_3_1.py
@@ -1,15 +1,17 @@
 #! /usr/bin/env python
 
 import rospy
+from iri_wam_reproduce_trajectory.srv import ExecTraj
 import rospkg
 rospack = rospkg.RosPack()
 traj = rospack.get_path('iri_wam_reproduce_trajectory') + \
-    "/config/get_food.txt"
-
+    "/config/init_pos.txt"
+# change the filename above to either "/config/get_food.txt"
+# or "/config/release_food.txt" to view the three different poses built in
 
 rospy.init_node('service_client')
 rospy.wait_for_service('/execute_trajectory')
 execute_trajectory_service = rospy.ServiceProxy(
-    '/execute_trajectory')
+    '/execute_trajectory', ExecTraj)
 
 print execute_trajectory_service(traj)
